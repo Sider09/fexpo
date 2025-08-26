c4859087a5a578b37f1973fb14bc8d2e0afe4a45
Sider09 <fabriciobedoyajk@gmail.com>
2025-08-26 01:40:48 -0400

    no funca todavia

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,14 +1,1 @@
-﻿import { View, Text } from "react-native";
-
-export default function MapTab() {
-  return (
-    <View className="flex-1 bg-neutral-100 p-4">
-      <View className="bg-white rounded-2xl p-5 shadow">
-        <Text className="text-xl font-semibold">Mapa</Text>
-        <Text className="text-neutral-600 mt-1">
-          Escaneá un stand y te diremos a qué pabellón ir.
-        </Text>
-      </View>
-    </View>
-  );
-}
+﻿export { default } from "./missions";
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 ﻿import { Stack } from "expo-router";
 import "../global.css";
-
 export default function RootLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
